117c22e1667bab2902eabf9b46a24d03b290df62
bskang <k333123@gmail.com>
2022-01-13 09:53:52 +0900

    upload

--- src/App.js
+++ src/App.js
@@ -5,26 +5,8 @@ import ReactDOM from 'react-dom';
 import {Grid, Box, Button} from '@material-ui/core'; 
 
 function App() {
-  return (
-    /*
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
-    <div>
-       
+  return ( 
+    <div> 
       <Grid container spacing={2}>
         <Grid item xs={8}>
           <Box bgcolor="info.main" color="info.contrastText" p={2}>
